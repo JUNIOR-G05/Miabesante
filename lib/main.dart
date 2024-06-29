e3762de6d2786b45fdff381db5a95ddf81248f9d
JUNIOR-G05 <guemba.badjonda@ipnetinstitute.com>
2024-06-29 22:06:32 +0000

    2er push

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:miabesante/firebase_options.dart';
 import 'package:miabesante/screens/welcome_screen.dart';
 
-void main(){
-  runApp(MyApp());
+Future<void> main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp( MyApp());
 
 }
 class MyApp extends StatelessWidget {
